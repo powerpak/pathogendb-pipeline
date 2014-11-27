dc91db5dcca1816cf21dd593911d172135778a5a
Theodore Pak <powerpak006@gmail.com>
2014-11-27 11:29:10 -0500

    fix MODULEPATH for users that don't have minerva-mothra in it

--- scripts/env.example.sh
+++ scripts/env.example.sh
@@ -13,3 +13,8 @@ export TMP="/sc/orga/scratch/$USER/tmp"
 export SMRTPIPE="/sc/orga/projects/InfectiousDisease/smrtpipe"
 export SMRTANALYSIS="/sc/orga/projects/pacbio/modules/smrtanalysis/2.2.0/install/smrtanalysis_2.3.0.140936"
 export SHARED_DIR="/sc/orga/scratch/$USER/shared_dir"
+
+# Ensures that the required module files are in MODULEPATH
+if [[ ":$MODULEPATH:" != *":/hpc/packages/minerva-mothra/modulefiles:"* ]]; then
+    export MODULEPATH="${MODULEPATH:+"$MODULEPATH:"}/hpc/packages/minerva-mothra/modulefiles"
+fi
