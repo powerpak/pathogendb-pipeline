5e3e7df3625aa314b44a66fdd144e1468f4677b0
Theodore Pak <powerpak006@gmail.com>
2015-02-18 20:18:08 -0500

    /gene and /inference tagging is working

--- scripts/improve_rast_gbk.rb
+++ scripts/improve_rast_gbk.rb
@@ -5,8 +5,11 @@ require 'optparse'
 require 'fileutils'
 require 'tempfile'
 require 'pp'
+require 'rexml/document'
 
 class ImproveRastOutput
+
+  REQUIRED_BINS = {"blastp"=>"BLAST+", "makeblastdb"=>"BLAST+"}
 
   def initialize(args)
     @options = {}
@@ -77,13 +80,15 @@ Improves default RAST genbank output by adding to annotated features as follows:
     end
     
     open_files(args)
-  end  # parse()
+  end
   
   def run!
     begin
       make_tmpdir
+      check_executables
       make_blast_db
-      print_features
+      match_genes
+      print_output
     ensure
       remove_tmpdir
     end
@@ -108,32 +113,121 @@ Improves default RAST genbank output by adding to annotated features as follows:
     @tmpdir = Dir.mktmpdir
   end
   
-  def make_blast_db
-    @refs.each do |ref|
-      ref.each do |record|
-        record.features.each do |feature|
-          gene_qualifier = feature.qualifiers.find{|q| q.qualifier == 'gene' }
-          translation_qualifier = feature.qualifiers.find{|q| q.qualifier == 'translation' }
-          protein_id_qualifier = feature.qualifiers.find{|q| q.qualifier == 'protein_id' }
-          if feature.feature == "CDS" && gene_qualifier
-            protein_id = protein_id_qualifier && protein_id_qualifier.value
-            if translation_qualifier
-              puts ">#{gene_qualifier.value} #{File.basename ref.path} #{ protein_id }"
-              puts translation_qualifier.value
-            else
-              STDERR.puts "WARN: No translation (AA sequence) given for #{gene_qualifier.value} in #{File.basename ref.path}"
-            end
-          end
-        end
-      end
+  def check_executables
+    if missing = REQUIRED_BINS.keys.find{|b| `which #{b}`.strip.size == 0 }
+      fail "FAIL: Could not find \`#{missing}\` in your $PATH; please ensure #{REQUIRED_BINS[missing]} is installed."
     end
   end
   
-  def print_features 
-    @rast_output.each do |record|
-      record.features.each do |feature|
-        #pp feature
-      end
+  def make_blast_db
+    File.open("#{@tmpdir}/refgenes.fastp", "w") do |f|
+      @refs.each do |ref|
+        ref.each do |record|
+          
+          record.features.each do |feature|
+            gene_qualifier = feature.qualifiers.find{|q| q.qualifier == 'gene' }
+            
+            next unless feature.feature == "CDS" && gene_qualifier
+            
+            translation_qualifier = feature.qualifiers.find{|q| q.qualifier == 'translation' }
+            protein_id_qualifier = feature.qualifiers.find{|q| q.qualifier == 'protein_id' }
+            
+            unless translation_qualifier
+              STDERR.puts "WARN: No translation (AA sequence) given for #{gene_qualifier.value} in #{File.basename ref.path}"
+              # TODO: should be able to get this anyway by iterating on feature.locations, grabbing nt's, translating manually
+              next
+            end
+            
+            protein_id = protein_id_qualifier && protein_id_qualifier.value
+            
+            f.write ">#{gene_qualifier.value} #{ protein_id } #{File.basename ref.path}\n"
+            f.write translation_qualifier.value
+            f.write "\n"
+          end
+          
+        end # ref.each
+      end # @refs.each
+    end
+    Dir.chdir(@tmpdir) do
+      `makeblastdb -in refgenes.fastp -dbtype prot`
+    end
+  end
+  
+  # The threshold for a blastp <Hit> becoming the annotated gene name for a CDS.
+  # Currently we base this on E-value and <Hit_score> (normalized to product length) thresholds.
+  def meets_threshold(hit, query)
+    hit && hit[:evalue] < 1e-10 && hit[:score].to_f / query.size > 3.5
+  end
+  
+  def match_genes
+    counts = {:all => 0, :searched => 0, :matched => 0}
+    @rast_output_records = []
+    
+    Dir.chdir(@tmpdir) do
+      
+      @rast_output.each do |record|
+        record.features.each do |feature|
+          counts[:all] += 1
+          db_xref_qualifier = feature.qualifiers.find{|q| q.qualifier == 'db_xref' }
+          translation_qualifier = feature.qualifiers.find{|q| q.qualifier == 'translation' }
+          
+          next unless feature.feature == "CDS" # TODO: should re-label tRNA and rRNA better too
+          unless translation_qualifier
+            STDERR.puts "WARN: No translation (AA sequence) given for #{db_xref_qualifier.value} in #{File.basename @rast_output.path}"
+            # TODO: could use the nt's gathered from feature.locations, grabbing exons, and then blastx -query_gencode 11
+            next
+          end
+          
+          counts[:searched] += 1
+          aa_query = translation_qualifier.value
+          File.open("query.fastp", 'w') {|f| f.write(aa_query) }
+          
+          xml = `blastp -db refgenes.fastp -query query.fastp -outfmt 5`
+          doc = REXML::Document.new(xml)
+          
+          hits = []
+          doc.elements.each('BlastOutput/BlastOutput_iterations/Iteration/Iteration_hits/Hit') do |hit_el|
+            hsps = []
+            
+            hit_el.elements.each('Hit_hsps/Hsp') do |hsp_el| 
+              hsps << {
+                :score => hsp_el.elements["Hsp_score"].text.to_i,
+                :evalue=> hsp_el.elements["Hsp_evalue"].text.to_f,
+                :midline => hsp_el.elements["Hsp_midline"].text
+              }
+            end
+            top_hsp = hsps.max_by{|a| a[:score] }
+            
+            hit_def = hit_el.elements['Hit_def'].text.split(/ /, 3)
+            hits << {:gene => hit_def[0], :protein_id => hit_def[1], :file => hit_def[2]}.merge(top_hsp)
+          end
+          top_hit = hits.min_by{|a| a[:evalue] }
+          
+          if top_hit && meets_threshold(top_hit, aa_query)
+            feature.qualifiers << Bio::Feature::Qualifier.new("gene", top_hit[:gene])
+            if top_hit[:protein_id]
+              protein_id_type = top_hit[:protein_id][2] == '_' ? "RefSeq" : "GenBank"
+              inference = "DESCRIPTION:similar to AA sequence (same species):blastp:#{protein_id_type}:#{top_hit[:protein_id]}"
+            else
+              inference = "DESCRIPTION:similar to AA sequence (same species):blastp:file:#{top_hit[:file]}"
+            end
+            feature.qualifiers << Bio::Feature::Qualifier.new("inference", inference)
+            
+            counts[:matched] += 1
+          end
+        end # record.features.each
+        
+        @rast_output_records << record
+      end # @rast_output.each
+      
+    end # Dir.chdir
+    
+    STDERR.puts "INFO: #{counts[:all]} features processed, #{counts[:searched]} searched, #{counts[:matched]} matched"
+  end
+  
+  def print_output
+    @rast_output_records.each do |record|
+      STDOUT.write record.to_biosequence.output(:genbank)
     end
   end
   
