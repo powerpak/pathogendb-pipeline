62e8dcf28b58979ccb9a6733bd25bf0947fa5cd0
Attie <attieo02@login1.mothra.hpc.mssm.edu>
2016-03-31 17:36:42 -0400

    modified:   xml_generator.py

--- scripts/xml_generator.py
+++ scripts/xml_generator.py
@@ -2,11 +2,12 @@ import sys
 bp=[]
 bs=[]
 reads=[]
+reads_dict={}
+bs_id=""
 BioProject_flag=0
 BioSample_flag=0
 Reads_flag=0
 datafile=sys.argv[1]
-
 fh=open(datafile, 'r')
 for line in fh:
     if(line.rstrip().split("\t")[0]=='<BioProject>'):
@@ -23,117 +24,142 @@ for line in fh:
         Reads_flag=0
     data_list=line.rstrip().split(",")
     if(BioProject_flag==1):
-        bp.append(data_list)
+        if(data_list[0]!='<BioProject>'):
+            bp.append(data_list)
     if(BioSample_flag):
-        bs.append(data_list)
+        if(data_list[0]!='<BioSample>'):
+            bs.append(data_list)
+            bs_id=data_list[0]
+            reads=[]
+#            print bs_id
+#        bs.append(data_list)
     if(Reads_flag):
-        reads.append(data_list)
+        if(data_list[0]!='<Reads>'):
+            reads.append(data_list)
+    reads_dict[bs_id]=reads
+#    print reads
 #print bp[1][0]
-print "<Submission xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"../../xml/portal/submission.xsd\">"
-print "\t<Description>"
-print "\t\t<Comment> BP(1.0)+BS(1.0)+SRA</Comment>"
-print "\t\t<Name>Icahn School of Medicine at Mount Sinai</Name>"
-print "\t\t<Contact email=\"oliver.attie@mssm.edu\">"
-print "\t\t<Name>"
-print "\t\t\t<First>Oliver</First>"
-print "\t\t\t<Last>Attie</Last>"
-print "\t\t</Name>"
-print "\t</Organization>"
-print "\t<Hold release_date=\'"+bp[1][0]+"\'></Hold>"
-print "\t</Description>"
-print "<Action>"
-print "\t<AddData target_db=\"BioProject\">"
-print "\t\t<Data content_type=\"xml\">"
-print "\t\t\t<XmlContent>"
-print "\t\t\t<Project schema_version=\"2.0\">"
-print "\t\t\t<ProjectID>"
-print "\t\t\t<<SPUID spuid_namespace=\"ISMMS_PSP\">"+bp[1][1]+"</SPUID>"
-print "\t\t\t</ProjectID>"
-print "\t\t\t<Descriptor>"
-print "\t\t\t<Title>"+bp[1][2]+"</Title>"
-print "\t\t\t<Description><p>"+bp[1][2]+"</p></Description>"
-print "\t\t\t<Relevance>"
-print "\t\t\t<Medical>Yes</Medical>"
-print "\t\t\t</Relevance>"
-print "\t\t</Descriptor>"
-print "\t\t<ProjectType>"
-print "\t\t<ProjectTypeSubmission sample_scope=\"eMonoisolate\">"
-print "\t\t\t<Organism>"
-print "\t\t\t<OrganismName>"+bp[1][3]+"</OrganimName>"
-print "\t\t</Organism>"
-print "\t\t<IntendedDataTypeSet>"
-print "\t\t\t<DataType>genome sequencing</DataType>"
-print "\t\t</IntendedDataTypeSet>"
-print "\t\t</ProjectTypeSubmission>"
-print "\t</ProjectType>"
-print "\t</Project>"
-print "\t</XmlContent>"
-print "</Data>"
-print "<Identifier>"
-print "\t<SPUID spuid_namespace=\"ISMMS_PSP\">"+bp[1][4]+"</SPUID>"
-print "</Identifier>"
-print "</AddData>"
-print "</Action>"
-print "<Action>"
-print "\t<AddData target_db=\"BioSample\">"
-print "\t\t<Data content_type=\"xml\">"
-print "\t\t\t<XmlContent>"
-print "\t\t\t<BioSample schema_version=\"2.0\">"
-print "\t\t\t<SampleId>"
-#print bs
-print "\t\t\t<SPUID spuid_namespace=\"ISMMS_PSP\">"+bs[1][0]+"</SPUID>"
-print "\t\t\t</SampleId>"
-print "\t\t\t<Descriptor>"
-print "\t\t\t<Title>"+bs[1][1]+"</Title>"
-print "\t\t\t</Descriptor>"
-print "\t\t\t<Organism>"
-print "\t\t\t<OrganismName>"+bs[1][2]+"</OrganismName>"
-print "\t\t\t</Organism>"
-print "\t\t\t<Package>Pathogen.env.1.0</Package>"
-print "\t\t\t<Attributes>"
-print "\t\t\t<Attribute attribute_name=\"strain\">"+bs[1][0]+"</Attribute>"
-print "\t\t\t<Attribute attribute_name=\"collected_by\">ISMMS</Attribute>"
-print "\t\t\t<Attribute attribute_name=\"collected_date\">"+bs[1][3]+"</Attribute>"
-print "\t\t\t<Attribute attribute_name=\"isolation_source\">"+bs[1][4]+"</Attribute>"
-print "\t\t\t<Attribute attribute_name=\"geo_loc_name\">USA:New York City</Attribute>"
-print "\t\t\t<Attribute attribute_name=\"lat_lon\">40N 73W</Attribute>"
-print "\t\t</Attributes>"
-print "\t\t</BioSample>"
-print "\t</XmlContent>"
-print "\t</Data>"
-print "\t<Identifier>"
-print "\t<SPUID spuid_namespace=\"ISMMS_PSP\">"+bs[1][0]+"</SPUID>"
-print "\t</Identifier>"
-print "</AddData>"
-print "</Action>"
-print "<Action>"
-print "\t<AddFiles target_db=\"SRA\">"
+
+print '''<Submission xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"../../xml/portal/submission.xsd\">
+ <Description>
+    <Comment> BP(1.0)+BS(1.0)+SRA</Comment>
+     <Organization role=\"owner\" type=\"institute\">
+      <Name>Icahn School of Medicine at Mount Sinai</Name>
+       <Contact email=\"oliver.attie@mssm.edu\">
+      <Name>
+      <First>Oliver</First>
+     <Last>Attie</Last>
+     </Name>
+     </Organization>
+'''
+print "\t<Hold release_date=\'"+bp[0][0]+"\'></Hold>"
+print ''' </Description>
+<Action>
+ <AddData target_db=\"BioProject\">
+   <Data content_type=\"xml\">
+     <XmlContent>
+      <Project schema_version=\"2.0\">
+       <ProjectID>
+'''
+print "\t\t\t<SPUID spuid_namespace=\"ISMMS_PSP\">"+bp[0][1]+"</SPUID>"
+print '''</ProjectID>
+        <Descriptor>
+'''
+print "\t\t\t<Title>"+bp[0][2]+"</Title>"
+print "\t\t\t<Description><p>"+bp[0][2]+"</p></Description>"
+print '''    <Relevance>
+             <Medical>Yes</Medical>
+             </Relevance>
+         </Descriptor>
+         <ProjectType>
+         <ProjectTypeSubmission sample_scope=\"eMonoisolate\">
+         <Organism>
+'''
+print "\t\t\t<OrganismName>"+bp[0][3]+"</OrganimName>"
+print '''  </Organism>
+            <IntendedDataTypeSet>
+             <DataType>genome sequencing</DataType>
+            </IntendedDataTypeSet>
+          </ProjectTypeSubmission>
+      </ProjectType>
+     </Project>
+   </XmlContent>
+</Data>
+<Identifier>
+'''
+print "\t<SPUID spuid_namespace=\"ISMMS_PSP\">"+bp[0][4]+"</SPUID>"
+print '''</Identifier>
+            </AddData>
+            </Action>
+           <Action>
+        <AddData target_db=\"BioSample\">
+         <Data content_type=\"xml\">
+          <XmlContent>
+'''
+#print len(bs)
+for i in range(0,len(bs)):
+    print '''     <BioSample schema_version=\"2.0\">
+                      <SampleId>
+              '''
+#    print i
+    print      "<SPUID spuid_namespace=\"ISMMS_PSP\">"+bs[i][0]+"</SPUID>"
+    print '''         </SampleId>
+                        <Descriptor>
+        '''
+    print "\t\t\t<Title>"+bs[i][1]+"</Title>"
+    print '''       </Descriptor>
+                     <Organism>'''
+    print "\t\t\t<OrganismName>"+bs[i][2]+"</OrganismName>"
+    print '''    </Organism>
+                 <Package>Pathogen.env.1.0</Package>
+                 <Attributes>'''
+    print "\t\t\t<Attribute attribute_name=\"strain\">"+bs[i][0]+"</Attribute>"
+    print '''<Attribute attribute_name=\"collected_by\">ISMMS</Attribute>
+              '''
+    print "\t\t\t<Attribute attribute_name=\"collected_date\">"+bs[i][3]+"</Attribute>"
+    print "\t\t\t<Attribute attribute_name=\"isolation_source\">"+bs[i][4]+"</Attribute>"
+    print '''    <Attribute attribute_name=\"geo_loc_name\">USA:New York City</Attribute>
+                  <Attribute attribute_name=\"lat_lon\">40N 73W</Attribute>"
+                  </Attributes>
+                </BioSample>
+              </XmlContent>
+             </Data>
+           <Identifier>
+           '''
+    print "\t<SPUID spuid_namespace=\"ISMMS_PSP\">"+bs[i][0]+"</SPUID>"
+    print ''' </Identifier>
+        </AddData>
+        </Action>
+        '''
+    print '''<Action>
+              <AddFiles target_db=\"SRA\">'''
+#print BioSample_flag
 #print reads
 #print len(reads)
-for i in range(1,len(reads)):
-    if i%2==1:
-        print "\t\t<File file_path=\""+reads[i][0]+"\">"
-        print "\t\t<DataType>generic-data</DataType>"
-        print "\t\t</File>"
-        print "\t\t<Attribute name=\"instrument_model\">"+reads[i][1]+"</Attribute>"
-        print "\t\t<Attribute name=\"library_name\">"+reads[i][2]+"</Attribute>"
-        print "\t\t<Attribute name=\"library_strategy\">WGS</Attribute>"
-        print "\t\t<Attribute name=\"library_source\">GENOMIC</Attribute>"
-        print "\t\t<Attribute name=\"library_selection\">RANDOM</Attribute>"
-        print "\t\t<Attribute name=\"library_layout\">FRAGMENTED</Attribute>"
-        print "\t\t<AttributeRefId name=\"BioProject\">"
-        print "\t\t<RefId>"
-        print "\t\t<SPUID spuid_namespace=\"ISMMS_PSP\">"+bs[1][0]+"</SPUID>"
-        print "\t\t</RefId>"
-        print "\t</AttributeRefId>"
-        print "\t<AttributeRefId name=\"BioSample\">"
-        print "\t\t<RefId>"
-        print "\t\t\t<SPUID spuid_namespace=\"ISMMS_PSP\">"+bs[1][0]+"</SPUID>"
-        print "\t\t</RefId>"
-        print "\t</AttributeRefId>"
-        print "\t<Identifier>"
-        print "\t\t<SPUID spuid_namespace=\"ISMMS_PSP\">"+reads[i][3]+"</SPUID>"
-        print "\t</Identifier>"
-        print "</AddFiles>"
-print "</Action>"
+    for j in range(0,len(reads_dict[bs[i][0]])):
+        print "\t\t<File file_path=\""+reads_dict[bs[i][0]][j][0]+"\">"
+        print '''<DataType>generic-data</DataType>
+        </File>'''
+
+    print "\t\t<Attribute name=\"instrument_model\">"+reads_dict[bs[i][0]][0][1]+"</Attribute>"
+    print "\t\t<Attribute name=\"library_name\">"+reads_dict[bs[i][0]][0][2]+"</Attribute>"
+    print '''<Attribute name=\"library_strategy\">WGS</Attribute>
+                 <Attribute name=\"library_source\">GENOMIC</Attribute>
+                 <Attribute name=\"library_selection\">RANDOM</Attribute>
+                 <Attribute name=\"library_layout\">FRAGMENTED</Attribute>
+                 <AttributeRefId name=\"BioProject\">
+                 <RefId>'''
+    print "\t\t<SPUID spuid_namespace=\"ISMMS_PSP\">"+bs[i][0]+"</SPUID>"
+    print '''</RefId>
+                 </AttributeRefId>
+                 <AttributeRefId name=\"BioSample\">
+                 <RefId>'''
+    print "\t\t\t<SPUID spuid_namespace=\"ISMMS_PSP\">"+bs[i][0]+"</SPUID>"
+    print '''</RefId>
+                 </AttributeRefId>
+                 <Identifier>'''
+    print "\t\t<SPUID spuid_namespace=\"ISMMS_PSP\">"+reads_dict[bs[i][0]][0][3]+"</SPUID>"
+    print '''</Identifier>
+        </AddFiles>
+     </Action>'''
 print "</Submission>"
