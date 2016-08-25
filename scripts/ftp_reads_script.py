89a4cac5159a88064d45b07cd20f90b1b926d0e6
Attie <attieo02@login1.mothra.hpc.mssm.edu>
2016-08-25 14:31:41 -0400

    modified:   ftp_reads_script.py

--- scripts/ftp_reads_script.py
+++ scripts/ftp_reads_script.py
@@ -22,8 +22,8 @@ def queryPathogenDB(organism, isolateID_list):
     results=[]
     db=MySQLdb.connect(host=args.host, db=args.database, user=args.user, passwd=args.password)
     cur=db.cursor()
-    for isolateID in isolateID_list:
-        cur.execute("select tIsolates.isolate_ID,tSequencing_runs.sequence_run_ID,tSequencing_runs.run_data_link from tSequencing_runs join tExtracts on tSequencing_runs.extract_ID=tExtracts.extract_ID join tStocks on tExtracts.stock_ID=tStocks.stock_ID join tIsolates on tStocks.isolate_ID=tIsolates.isolate_ID join tOrganisms on tIsolates.organism_ID=tOrganisms.organism_ID where tOrganisms.full_name=\'"+organism+"\' and tIsolates.isolate_ID=\'"+isolateID+"\'")
+    for extractID in isolateID_list:
+        cur.execute("select tIsolates.isolate_ID,tSequencing_runs.sequence_run_ID,tSequencing_runs.run_data_link from tSequencing_runs join tExtracts on tSequencing_runs.extract_ID=tExtracts.extract_ID join tStocks on tExtracts.stock_ID=tStocks.stock_ID join tIsolates on tStocks.isolate_ID=tIsolates.isolate_ID join tOrganisms on tIsolates.organism_ID=tOrganisms.organism_ID where tOrganisms.full_name=\'"+organism+"\' and tExtracts.extract_ID=\'"+extractID+"\'")
         for row in cur.fetchall():
             results.append(row)
         db.close()
@@ -48,17 +48,19 @@ results=queryPathogenDB(args.organism, isolate_ID_list)
 
 run_data=fill_data(results)
 
-ftp=FTP(args.ncbi)
-ftp.login(args.service, args.ftp_password)
-ftp.cwd(args.directory)
+
+
+
 
 for isolate in run_data.keys():
     for exp in run_data[isolate].keys():
         url=re.split("/", run_data[isolate][exp])
         fh=open("/sc/orga/projects/pacbio/userdata_permanent/jobs/"+str(url[-1])[:3]+"/"+url[-1]+"/input.fofn", 'r')
         for line in fh.readlines():
+            ftp=FTP(args.ncbi)
+            ftp.login(args.service, args.ftp_password)
+            ftp.cwd(args.directory)
             reads=line.rstrip().split("/")
             fh=open(line.rstrip(),'r')
             ftp.storbinary("STOR "+reads[-1], fh)
-
-ftp.close()
+            ftp.close()
